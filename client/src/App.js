b950ec9bc3fe4f1db32d421f3f4cbb9dc11a5c77
BhushanVnit <bhushanbahale01@gmail.com>
2023-01-28 23:45:07 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App text-blue-500">
+        <Nav/>
+      <p>Hello Acc</p>
+    </div>
+  );
+}
+
+export default App;
